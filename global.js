e36d89641353b49e45678b95768aa324b430682a
exit <shuadian1@gmail.com>
2016-01-16 04:12:48 +0800

    v1.0
    v1.0

---
+++ global.js
@@ -0,0 +1,20 @@
+$(function(){
+	/*$('#menu-more').click(function(){
+		if(  $(this).attr('visible')== 'on'){
+			$('.card').addClass('hide');
+			$('.header-bottom').addClass('hide');
+			$('footer').show();
+			 $(this).attr('visible','off');
+			return false;
+		}
+		$('.card').removeClass('hide');
+		$('.header-bottom').removeClass('hide');
+		$('footer').hide();
+		$(this).attr('visible','on');
+	});
+	
+	$('footer a').mousedown(function(){
+		console.log('mousedown');
+		$(this).css({'color':'white'}).parent().css({'background-color':'#FFBF00'});
+	});*/
+});
